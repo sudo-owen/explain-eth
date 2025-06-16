79a6237212f8becc4905afca4e5b99b1dc90efd3
owen <22165146+sudo-owen@users.noreply.github.com>
2025-06-16 14:10:11 -0700

    fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,28 @@
-import { useState, useEffect, useRef } from 'react'
-import ChainColumn from './components/ChainColumn'
-import TransactionModal from './components/TransactionModal'
-import TransactionHistoryOverlay from './components/TransactionHistoryOverlay'
-import BridgeOverlay from './components/BridgeOverlay'
-import { useBlockchain } from './hooks/useBlockchain'
-import { Recipient } from './types/blockchain'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import IntroPage from './pages/IntroPage'
+import AppsPage from './pages/AppsPage'
+import TokensPage from './pages/TokensPage'
+import Apps2Page from './pages/Apps2Page'
+import PlaygroundPage from './pages/PlaygroundPage'
+import { BlockchainProvider } from './contexts/BlockchainContext'
 
 function App() {
-  const [isHistoryOpen, setIsHistoryOpen] = useState(true)
-  const [isBridgeOpen, setIsBridgeOpen] = useState(false)
-  const previousTransactionCount = useRef(0)
-
-  const {
-    ethereumState,
-    rollupState,
-    sendMoney,
-    purchaseNFT,
-    sellNFT,
-    depositEarnings,
-    withdrawEarnings,
-    claimEarnings,
-    bridgeToRollup,
-    calculateCurrentEarnings,
-    transactionHistory,
-    modalState,
-    currentPendingTransaction,
-    closeModal
-  } = useBlockchain()
-
-  // Auto-open overlay when new transaction is submitted
-  useEffect(() => {
-    if (transactionHistory.length > previousTransactionCount.current) {
-      setIsHistoryOpen(true)
-    }
-    previousTransactionCount.current = transactionHistory.length
-  }, [transactionHistory.length])
-
   return (
-    <div className="min-h-screen bg-gray-900 text-white">
-
-      {/* Main Content */}
-      <div className="flex flex-col lg:flex-row min-h-[calc(100vh-120px)]">
-        {/* Ethereum Mainnet */}
-        <ChainColumn
-          title="Ethereum Mainnet"
-          chainState={ethereumState}
-          onSendMoney={(recipient: Recipient, amount: number) => sendMoney('ethereum', recipient, amount)}
-          onPurchaseNFT={(nftId: string, price: number, emoji: string) => purchaseNFT('ethereum', nftId, price, emoji)}
-          onSellNFT={(nftId: string) => sellNFT('ethereum', nftId)}
-          onDepositEarnings={(amount: number) => depositEarnings('ethereum', amount)}
-          onWithdrawEarnings={(amount: number) => withdrawEarnings('ethereum', amount)}
-          onClaimEarnings={() => claimEarnings('ethereum')}
-          currentEarnings={calculateCurrentEarnings(ethereumState)}
-          theme="ethereum"
-          className="lg:w-1/2 border-r border-gray-700"
-          onBridgeToggle={() => setIsBridgeOpen(!isBridgeOpen)}
-        />
-
-        {/* Rollup */}
-        <ChainColumn
-          title="Rollup"
-          chainState={rollupState}
-          onSendMoney={(recipient: Recipient, amount: number) => sendMoney('rollup', recipient, amount)}
-          onPurchaseNFT={(nftId: string, price: number, emoji: string) => purchaseNFT('rollup', nftId, price, emoji)}
-          onSellNFT={(nftId: string) => sellNFT('rollup', nftId)}
-          onDepositEarnings={(amount: number) => depositEarnings('rollup', amount)}
-          onWithdrawEarnings={(amount: number) => withdrawEarnings('rollup', amount)}
-          onClaimEarnings={() => claimEarnings('rollup')}
-          currentEarnings={calculateCurrentEarnings(rollupState)}
-          theme="rollup"
-          className="lg:w-1/2 bg-gradient-to-br from-purple-950/20 to-indigo-950/20"
-        />
-      </div>
-
-      {/* Transaction Modal */}
-      {modalState.isOpen && (
-        <TransactionModal
-          isOpen={modalState.isOpen}
-          type={modalState.type}
-          message={modalState.message}
-          onClose={closeModal}
-          pendingTransaction={currentPendingTransaction}
-        />
-      )}
-
-      {/* Transaction History Overlay */}
-      <TransactionHistoryOverlay
-        ethereumTransactions={transactionHistory.filter(tx => tx.chain === 'ethereum')}
-        rollupTransactions={transactionHistory.filter(tx => tx.chain === 'rollup')}
-        ethereumPendingCount={ethereumState.pendingTransactions}
-        rollupPendingCount={rollupState.pendingTransactions}
-        isOpen={isHistoryOpen}
-        onToggle={() => setIsHistoryOpen(!isHistoryOpen)}
-      />
-
-      {/* Bridge Overlay */}
-      <BridgeOverlay
-        ethereumState={ethereumState}
-        rollupState={rollupState}
-        onBridge={bridgeToRollup}
-        isOpen={isBridgeOpen}
-        onToggle={() => setIsBridgeOpen(!isBridgeOpen)}
-      />
-    </div>
+    <BrowserRouter>
+      <BlockchainProvider>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/intro" element={<IntroPage />} />
+          <Route path="/apps" element={<AppsPage />} />
+          <Route path="/tokens" element={<TokensPage />} />
+          <Route path="/apps2" element={<Apps2Page />} />
+          <Route path="/playground" element={<PlaygroundPage />} />
+          {/* Catch-all route - redirect any unmatched routes to /intro */}
+          <Route path="*" element={<Navigate to="/intro" replace />} />
+        </Routes>
+      </BlockchainProvider>
+    </BrowserRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import HomePage from './pages/HomePage.tsx'
-import IntroPage from './pages/IntroPage.tsx'
-import AppsPage from './pages/AppsPage.tsx'
-import TokensPage from './pages/TokensPage.tsx'
-import Apps2Page from './pages/Apps2Page.tsx'
-import PlaygroundPage from './pages/PlaygroundPage.tsx'
-import { BlockchainProvider } from './contexts/BlockchainContext.tsx'
+import App from './App'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <BlockchainProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/intro" element={<IntroPage />} />
-          <Route path="/apps" element={<AppsPage />} />
-          <Route path="/tokens" element={<TokensPage />} />
-          <Route path="/apps2" element={<Apps2Page />} />
-          <Route path="/playground" element={<PlaygroundPage />} />
-          {/* Catch-all route - redirect any unmatched routes to /intro */}
-          <Route path="*" element={<Navigate to="/intro" replace />} />
-        </Routes>
-      </BlockchainProvider>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
